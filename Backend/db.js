bdcac3052a66cfdf9fc1d2f9aecf336a24ed2b54
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2023-06-07 19:48:29 +0530

    added

--- Backend/db.js
+++
@@ -1,9 +0,0 @@
-// const { connect } = require("mongoose");
-require("dotenv").config()
-const mongoose = require("mongoose")
-
-let connection = mongoose.connect(process.env.URL)
-
-module.exports = {
-    connection
-}
